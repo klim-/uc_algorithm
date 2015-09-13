0c020ca98269aafbec1a354954025bc396cb4dcc
Klemens Fritzsche <git@leckstrom.de>
2015-09-13 15:41:22 +0200

    added missing licence

--- franke_algorithm.py
+++ franke_algorithm.py
@@ -1,8 +1,27 @@
 # -*- coding: utf-8 -*-
 
+#    Copyright (C) 2015
+#    by Klemens Fritzsche, 2e63a67d46@leckstrom.de
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
-Erste Implementierung des Algorithmus zur Konstruktion flacher Ausgänge für
-nichtlineare Systeme von Dr. Matthias Franke, TU Dresden
+Implementation of an algorithm for determining differential flat ouputs
+for nonlinear dynamic systems (see Franke, Matthias: 'Über die
+Konstruktion flacher Ausgänge für nichtlineare Systeme und zur
+Polzuweisung durch statische Ausgangsrückführungen', Publisher: Logos
+Verlag Berlin, 2014).
 """
 
 # enable true divison
