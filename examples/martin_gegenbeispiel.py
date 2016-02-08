f6ddb8d1711c630db92b115a9eb5ab9afbe3c321
Klemens Fritzsche <git@leckstrom.de>
2016-02-08 17:16:20 +0100

    minor changes to martins example

--- examples/martin_gegenbeispiel.py
+++ examples/martin_gegenbeispiel.py
@@ -7,11 +7,13 @@ import symb_tools as st
 # set up the system
 s = sp.symbols("s")
 
-x1, x2, x3, x4, x5, x6, x7 = sp.symbols("x1, x2, x3, x4, x5, x6, x7")
-vec_x = sp.Matrix([x1, x2, x3, x4, x5, x6, x7])
-
+vec_x = st.symb_vector('x1:8', commutative=True)
 vec_xdot = st.perform_time_derivative(vec_x, vec_x)
-xdot1, xdot2, xdot3, xdot4, xdot5, xdot6, xdot7 = vec_xdot
+vec_xddot = st.perform_time_derivative(vec_xdot, vec_xdot)
+
+st.make_global(vec_x, 1)
+st.make_global(vec_xdot, 1)
+st.make_global(vec_xddot, 1)
 
 
 F_eq = sp.Matrix([
@@ -20,4 +22,9 @@ F_eq = sp.Matrix([
 [ -x7 - x5*xdot1 - x7*xdot3 + (x4 + xdot1)*x4*xdot1 - xdot6 ],
 [ x4 + xdot1 - xdot7 ]])
 
+
+Q = sp.Matrix([
+[x4*x7*xdot4*(x4*x7*xdot1 + 2*x4*xddot1 + 2*x4*xdot4 + 2*xdot1*xdot4 + xdot3 - xdot4*xdot7 - xdot5 + 1) + x4*xdot4*(x4**2*xdot7 - x4*x7**2*xdot1 - 2*x4*x7*xddot1 - 2*x4*x7*xdot4 + x4*xdot1*xdot7 + x4*xdot7**2 - x5*xdot7 - 2*x7*xdot1*xdot4 - x7*xdot3 + x7*xdot4*xdot7 + x7*xdot5 - x7) + xdot1*xdot4*(2*x4 + xdot1 - xdot7)*(x4*xdot7 + x4*(x4 + xdot1) - x5) - xdot4*(x4*xdot1 + x4*(x4 + xdot1) - x5)*(x4*xdot7 + x4*(x4 + xdot1) - x5), -xdot4*(x4**2*xdot7 - x4*x7**2*xdot1 - 2*x4*x7*xddot1 - 2*x4*x7*xdot4 + x4*xdot1*xdot7 + x4*xdot7**2 - x5*xdot7 - 2*x7*xdot1*xdot4 - x7*xdot3 + x7*xdot4*xdot7 + x7*xdot5 - x7), x7*xdot4*(x4*xdot7 + x4*(x4 + xdot1) - x5), 0, -xdot4*(x4*x7*xdot1 + 2*x4*xddot1 + 2*x4*xdot4 + 2*xdot1*xdot4 + xdot3 - xdot4*xdot7 - xdot5 + 1), xdot4*(x4*xdot7 + x4*(x4 + xdot1) - x5), -xdot1*xdot4*(2*x4 + xdot1 - xdot7)*(x4*xdot7 + x4*(x4 + xdot1) - x5)],
+[                                                                                                                                                                                                                                                                                                                                                                                                              x4*xdot1 + x4*(x4 + xdot1) - x5,                                                                                                                                                                               0,                                        -x7, 0,                                                                                                 0,                                      -1,                                                                     0],
+[                                                                                                                                                                                                                                                                                                                                                                                                                                            1,                                                                                                                                                                               0,                                          0, 0,                                                                                                 0,                                       0,                                                                    -1]])
 
