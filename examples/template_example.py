94b44a8c8a89a704b3bc6e1a646f425b6906fbc6
Klemens Fritzsche <git@leckstrom.de>
2016-02-22 17:08:31 +0100

    template example

---
+++ examples/template_example.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import sympy as sp
+import symb_tools as st
+from sympy import sin, cos, tan
+
+# Number of state variables
+n = 6 
+
+vec_x = st.symb_vector('x1:%i' % (n+1))
+vec_xdot = st.perform_time_derivative(vec_x, vec_x)
+st.make_global(vec_x, 1)
+st.make_global(vec_xdot, 1)
+
+# Additional symbols
+g, l = sp.symbols("g, l")
+
+# Time-dependent symbols
+diff_symbols = sp.Matrix([l])
+
+# Nonlinear system in state space representation 0 = F_eq
+F_eq = sp.Matrix([
+        [ xdot1 - x4 ],
+        [ xdot2 - x5 ],
+        [ xdot3 - x6 ],
+        [ g*sin(x1) + xdot4*x3 + 2*x4*x6 + xdot5*cos(x1)  ]])
+
+# Container carrying additional information about the example
+# (will be stored in pickle-file)
+data = st.Container()
+data.F_eq = F_eq
+data.time_dep_symbols = diff_symbols
