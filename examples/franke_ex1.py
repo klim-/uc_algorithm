9d6f6b6a978db33ee6ccd7473a39c67ec9157a49
Klemens Fritzsche <git@leckstrom.de>
2016-03-22 10:14:08 +0100

    changed ex1

--- examples/franke_ex1.py
+++ examples/franke_ex1.py
@@ -10,4 +10,7 @@ vec_xdot = st.time_deriv(vec_x, vec_x)
 st.make_global(vec_x, 1)
 st.make_global(vec_xdot, 1)
 
-F_eq = sp.Matrix([ xdot1 - x3*x4, xdot2 - x4, xdot3 - x5 ])
+F_eq = sp.Matrix([
+        [-x3*x4 + xdot1],
+        [-x4 + xdot2],
+        [-x5 + xdot3]])
